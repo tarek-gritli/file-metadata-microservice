6b4f0c1f6de9d7dd5f799ebf6ad5e4befdcbfde6
tarek-gritli <tarek.gritli@insat.ucar.tn>
2024-04-03 17:06:56 +0100

    Complete project

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 var express = require('express');
 var cors = require('cors');
+const multer = require('multer')
+const upload = multer({ dest: 'uploads/' })
 require('dotenv').config()
 
 var app = express();
@@ -11,7 +13,10 @@ app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
-
+app.post('/api/fileanalyse',upload.single('upfile'),(req,res)=>{
+  const {originalname,mimetype,size}=req.file;
+  res.status(200).json({name: originalname, type: mimetype, size})
+})
 
 
 const port = process.env.PORT || 3000;
